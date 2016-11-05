32ff05242cc471f32b77ef2ad7b148ecdafa8c29
Bill Hunt <bill@krues8dr.com>
2016-11-05 15:36:05 -0400

    Adding initial files

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # local-asset-importer
 Wordpress plugin to download linked assets from a given domain and replace them with a local copy.
+
+Uses the same idea as (and some code from) the fantastic [Image Teleporter plugin](https://github.com/internetmedicineman/image-teleporter/) but can handle other assets types.
+
+[GPLv3](LICENSE)
---
+++ local-asset-importer.php
@@ -0,0 +1,193 @@
+<?php
+/*
+Plugin Name: Local Asset Importer
+Plugin URI: https://github.com/krues8dr/local-asset-importer
+Description: Downloads assets from a given host and replaces them with a local copy. Inspired by Image Teleporter.
+Version: 1.0.0
+Author: Krues8dr
+Author URI: http://www.krues8dr.com/
+License: GPL3
+*/
+
+// require_once(ABSPATH . 'wp-admin/includes/file.php');
+// require_once(ABSPATH . 'wp-admin/includes/media.php');
+
+
+/** Step 2 (from text above). */
+add_action( 'admin_menu', 'lai_plugin_menu' );
+
+/** Step 1. */
+function lai_plugin_menu() {
+  add_submenu_page('tools.php', 'Local Asset Importer', 'Local Asset Importer', 'manage_options', 'local-asset-importer', 'lai_runner' );
+}
+
+/** Step 3. */
+function lai_runner() {
+  if ( !current_user_can( 'manage_options' ) )  {
+    wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+  }
+  echo '<h1>' . __('Local Asset Importer') . '</h1>';
+
+  lai_styles();
+  lai_handle_assets();
+  lai_show_form();
+}
+
+function lai_handle_assets() {
+
+  if(!isset($_GET['url']) || $_GET['url'] == '') {
+    return;
+  }
+
+  $url = $_GET['url'];
+  $limit = $_GET['limit'];
+  $offset = 0;
+  $status = 'publish';
+
+  global $wpdb;
+
+  $search_query = "SELECT ID, post_content FROM $wpdb->posts WHERE post_content LIKE '%{$url}%' ";
+  if($status) {
+    $search_query .= " AND post_status='$status' ";
+  }
+  $search_query .= " LIMIT $offset, $limit ";
+
+  $url = 'assets.sunlightfoundation.com';
+
+  $results = $wpdb->get_results($search_query, OBJECT );
+  foreach($results as $result) {
+    print "Updating $result->ID<ul>";
+
+    $content = $result->post_content;
+
+    preg_match_all('/(?P<attr>src|href)=([\'"])(?P<url>https?:\/\/'. $url .'\/(.*?))\2/', $result->post_content, $matches, PREG_SET_ORDER);
+    foreach($matches as $match) {
+      $new_filename = lai_store_file($match['url'], $result->ID);
+
+      print "<li>Copied {$match['url']} -> $new_filename</li>";
+
+      $content = lai_fix_content($match, $new_filename, $content);
+    }
+
+    wp_update_post(
+      array(
+        'ID' => $result->ID,
+        'post_content' => $content
+      )
+    );
+
+    print "</ul>";
+    print "Fixed content.<br><br>";
+  }
+}
+
+function lai_fix_content($match, $new_filename, $content) {
+  // Use a relative url;
+  $url_parts = parse_url($new_filename);
+  $new_url = $url_parts['path'];
+  if(isset($url_parts['query']) && $url_parts['query']) {
+     $new_url .= '?' . $url_parts['query'];
+  }
+
+  return str_replace(
+    $match[0],
+    $match['attr'] . '="' . $new_url . '"',
+    $content
+  );
+}
+
+function lai_store_file($path, $post_id) {
+    $content = file_get_contents($path);
+
+    $url_parts = explode('/', $path);
+    $filename = end($url_parts);
+
+    return lai_savefile($content, $filename, $post_id);
+}
+
+/*
+ * Copied directly from Image Teleporter.
+ * https://github.com/internetmedicineman/image-teleporter/
+ */
+function lai_savefile($file, $url, $post_id) {
+  $time = null;
+
+  $uploads = wp_upload_dir($time);
+  $filename = wp_unique_filename( $uploads['path'], $url);
+  $savepath = $uploads['path'] . "/$filename";
+
+  if($fp = fopen($savepath, 'w')) {
+    fwrite($fp, $file);
+    fclose($fp);
+  }
+
+  $wp_filetype = wp_check_filetype( $savepath );
+  $type = $wp_filetype['type'];
+  $title = $filename;
+  $content = '';
+
+  // Construct the attachment array
+  $attachment = array(
+            'post_mime_type' => $type,
+            'guid' => $uploads['url'] . "/$filename",
+            'post_parent' => $post_id,
+            'post_title' => $title,
+            'post_content' => $content
+            );
+
+  // Save the data
+  $id = wp_insert_attachment($attachment, $savepath, $post_id);
+  if ( !is_wp_error($id) ) {
+    wp_update_attachment_metadata( $id, wp_generate_attachment_metadata( $id, $file ) );
+  } else return '';
+  return $uploads['url'] . "/$filename";
+}
+
+function lai_styles() {
+  echo '<style>
+.lai-form-row label {
+  width: 150px;
+  display: inline-block;
+}
+.lai-form-row input[type=text] {
+}
+</style>';
+}
+
+function lai_show_form() {
+  $limit = 25;
+  if(isset($_GET['limit'])) {
+    $limit = $_GET['limit'];
+  }
+
+  // $offset = 0;
+  // if(isset($_GET['offset'])) {
+  //   $offset = $_GET['offset'];
+  // }
+
+  $url = '';
+  if(isset($_GET['url'])) {
+    $url = $_GET['url'];
+  }
+
+  echo '<form method="GET" class="lai-form">
+
+    <div class="lai-form-row">
+      <label for="url">URL</label>
+      <input type="text" name="url" id="url" value="' . $url . '">
+    </div>' .
+
+    // '<div class="lai-form-row">
+    //   <label for="offset">Start At</label>
+    //   <input type="text" name="offset" id="offset" value="' . $offset . '">
+    // </div>' .
+
+    '<div class="lai-form-row">
+      <label for="limit">Number of Entries</label>
+      <input type="text" name="limit" id="limit" value="' . $limit . '">
+    </div>
+
+    <input type="hidden" name="page" value="local-asset-importer">
+    <button name="submit" id="submit" class="button-primary">Go</button>
+  </form>';
+}
